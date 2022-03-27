3571f9a4db5de206f91eb2425768ec5f64dc7833
Rafa-Pires <rpiresoliver@gmail.com>
2022-03-27 15:43:37 -0300

    correcao script - identacao

--- scrappingSmercado.R
+++ scrappingSmercado.R
@@ -27,11 +27,8 @@ for (i in seq_along(results)) {
   url_menu <- 
     results[i]%>%
     html_attr("href",trimws(TRUE))
-  
   id=str_extract(url_menu,"[0-9]+")
-  
   if(is.na(id)==TRUE){next}
-  
   if (is.na(url_menu)==FALSE & id != idref) {
     categorias[[nr]] <- url_menu
     nr=nr+1
@@ -47,16 +44,13 @@ tabelaFinal <-
 for (a in seq_along(categorias)) {
   webpage <- 
     read_html(str_replace(paste("https://www.superpaguemenos.com.br",categorias[a]),' ',''))
-  
   # Os produtos dentro das categorias podem estar em várias páginas, 
   # Por isso primeiro pego a quantidade de paginas e depois faço um loop
   # coletando os preços dos produtos pg por pg
-  
   # número de páginas
   results <- 
     webpage%>% 
     html_nodes(".pagination")%>% html_nodes("li")%>% html_nodes("a")
-  
   qtd_pgs=0
   for (b in seq_along(results)) {
     if (results[b]%>%
@@ -64,21 +58,17 @@ for (a in seq_along(categorias)) {
       qtd_pgs=qtd_pgs+1
     }
   }
-  
   # Formando Data Frame com os produtos e valores
   nrpg=1
   for (c in c(1:qtd_pgs)) {
     webpage_b <- 
       read_html(str_replace_all(paste("https://www.superpaguemenos.com.br",categorias[a],"?p=",nrpg),' ',''))
-    
     results <- 
       webpage_b %>% 
       html_nodes(".item-product")%>% 
       html_nodes(".desc")
-    
     records <- 
       vector("list", length = length(results))
-    
     # listar produtos
     for (d in seq_along(results)) {
       CATEGORIA <- 
@@ -92,7 +82,6 @@ for (a in seq_along(categorias)) {
         html_nodes(".prices")%>% 
         html_nodes(".price")%>%
         html_text(trim = TRUE)
-      
       records[[d]] <- 
         data_frame(CATEGORIA = CATEGORIA, 
                    PRODUTO = PRODUTO, 
